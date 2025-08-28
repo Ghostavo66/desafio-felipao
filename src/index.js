2770e7f45b46fe675255fafb9900d1ab3f0c256f
ghostavo66 <gustavo.rb.1999@gmail.com>
2025-08-28 12:58:53 -0300

    código finalizado!

--- src/index.js
+++ src/index.js
@@ -1,23 +1,24 @@
 //desafio do felipão
 let nome = "Ghostavo"
-let xp = 1999
+let nivel
+for(let xp = 0; xp < 2000; xp+=100){
 
 if (xp < 1000) {
-    xp = "Ferro"
-} else if (xp > 1001 && xp < 2000){
-    xp = "Bronze"
+    nivel = "Ferro"
+} else if (xp >= 1001 && xp <= 2000){
+    nivel = "Bronze"
 } else if (xp > 2001 && xp < 5000){
-    xp = "Prata"
+    nivel = "Prata"
 } else if (xp > 5001 && xp < 7000){
-    xp = "Ouro"
+    nivel = "Ouro"
 } else if (xp > 7001 && xp < 8000){
-    xp = "Platina"
+    nivel = "Platina"
 } else if (xp > 8001 && xp < 9000){
-    xp = "Ascendente"
+    nivel = "Ascendente"
 } else if (xp > 9001 && xp <10000){
-    xp = "Imortal"
+    nivel = "Imortal"
 } else if (xp >= 10001){
-    xp = "Radiante"
+    nivel = "Radiante"
 }
-
-console.log("O Herói de nome " + nome + " está no nível de " + xp)
+}
+console.log("O Herói de nome " + nome + " está no nível de " + nivel)
